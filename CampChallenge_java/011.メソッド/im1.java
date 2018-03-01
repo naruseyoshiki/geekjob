3b282c1ae9db3c66c2efd4a96eabfabc8b00e477
naruseyoshiki <kirby.dedede.427@gmail.com>
2018-03-01 17:42:40 +0900

    課題提出「引数と戻り値」引数、戻り値1

---
+++ CampChallenge_java/011.メソッド/im1.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class im1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    String[] modoriti(String p){
+        String[] prof ={"123", "1996-01-01", "tokyo"};
+        String[] prof2={"789", "1996-01-03", "saitama"};
+        String[] prof1={"456", "1996-01-02", "kanagawa"};
+       if(p.equals(prof[0]) ){
+        return prof;
+    }else if(p.equals(prof2[0])){
+        return prof2;
+    }else if(p.equals(prof1[0])){
+        return prof1;
+    }else{
+        return prof1;
+    }
+    }
+
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String ID = "123";        
+            String[] p = modoriti(ID); //呼出し後：String [] p = prof1;
+
+            for (int i = 1; i <= 2; i++) {
+                out.print(p[i] + "<br>");
+            
+        }
+    }
+   }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
