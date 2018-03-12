004c397f786d2b69678dc95e5a159560450ac183
naruseyoshiki <kirby.dedede.427@gmail.com>
2018-03-12 11:52:57 +0900

    課題提出「クラスの継承」

---
+++ CampChallenge_java/013.クラス/Class3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Class3 extends Class1 {  //「クラスの作成」の親クラスから継承
+
+    protected void demo() {           //独自メソッド
+        name = null;                  //各値をクリアする
+        age = 0;
+    
+    }
+}
