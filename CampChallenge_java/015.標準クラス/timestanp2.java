6ac6bc94a98364eb71af1b0a7cd02cb98cd454e1
naruseyoshiki <kirby.dedede.427@gmail.com>
2018-03-22 14:35:44 +0900

    課題提出「標準クラス」タイムスタンプの表示(差)

---
+++ CampChallenge_java/015.標準クラス/timestanp2.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class timestanp2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            //Calendarクラスのインスタンス生成（C）
+            Calendar C = Calendar.getInstance();
+
+            //Cに具体的な日付をセット
+            C.set(2015, 0, 1, 0, 0, 0);
+
+            //Date型DでCのタイムスタンプの作成
+            Date D = C.getTime();
+
+            //Calendarクラスのインスタンス生成（C2）
+            Calendar C2 = Calendar.getInstance();
+
+            //C2に具体的な日付をセット
+            C2.set(2015, 11, 31, 23, 59, 59);
+
+            //Date型D2でC2のタイムスタンプの作成
+            Date D2 = C2.getTime();
+
+            //D2からDの時間を引くとその差が残る
+            out.print(D2.getTime() - D.getTime());
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
