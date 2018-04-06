ff5acafe1d0ef8e441e7fc02155abefa76276f91
naruseyoshiki <kirby.dedede.427@gmail.com>
2018-04-06 16:30:42 +0900

    DB操作-特定のレコード削除するフォーム

---
+++ CampChallenge_java/018.DB操作/特定のレコードの削除をするフォーム/profilesidformdelete.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class profilesidformdelete extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            //フォームの送信情報の書式設定   
+            request.setCharacterEncoding("UTF-8");
+
+            //[送信した値]を文字列変数に格納
+            String ID = request.getParameter("id");
+
+            Connection dbcon = null;
+            try {
+                //ドライバ接続
+                Class.forName("com.mysql.jdbc.Driver");
+
+                //DB接続
+                dbcon = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db", "naruse", "1015");
+
+                //フォームのprofilesIDで削除するSQL文を格納
+                String mozi = "delete from profiles where profilesID =?";
+
+                //SQL文を格納
+                PreparedStatement dbps = dbcon.prepareStatement(mozi);
+
+                //SQL文の?にフォームから[送信した値]を格納
+                dbps.setString(1, ID);
+
+                //DB更新
+                dbps.executeUpdate();
+
+                //表示SQL文を格納
+                String mozi1 = "select * from profiles";
+
+                //SQL文を実行
+                ResultSet dbrs = dbps.executeQuery(mozi1);
+
+                while (dbrs.next()) {
+                    out.print("<br>profilesID:" + dbrs.getInt("profilesID")
+                            + "<br>name:" + dbrs.getString("name")
+                            + "<br>tel:" + dbrs.getString("tel")
+                            + "<br>age:" + dbrs.getInt("age")
+                            + "<br>birthday:" + dbrs.getDate("birthday"));
+
+                }
+                dbcon.close();
+                dbps.close();
+                dbrs.close();
+
+            } catch (SQLException sqle) {
+                out.print("エラーが発生しました。" + sqle.toString());
+            } catch (Exception ex) {
+                out.print("エラーが発生しました！" + ex.toString());
+            } finally {
+                if (dbcon != null) {
+                    try {
+                        dbcon.close();
+                    } catch (Exception e) {
+                        out.print(e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
